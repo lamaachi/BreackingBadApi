7ab24147e94ffe2adae26ad14c8f58ddb996bbb0
YoussefLamaachi <76207603+lamaachi@users.noreply.github.com>
2022-06-12 23:17:20 +0200

    Load files bgImage/index/script

---
+++ script.js
@@ -0,0 +1,30 @@
+//fetch("https://www.breakingbadapi.com/api/")
+//.catch(res => res.json())
+//.then(data => console.log(data))
+
+async function getData(){ 
+    const respense = await fetch("https://www.breakingbadapi.com/api/characters")
+    const data = await respense.json()
+    console.log(data)
+    let characters = [];
+
+    //set names in options
+    data.forEach(element => {
+        characters.push(element.name)
+        document.querySelector('#actor-name').innerHTML += `<option value="${element.name}">${element.name}</option>`;
+    });
+
+    let select = document.querySelector('select');
+    select.onchange = (e)=>{
+        data.forEach(element=>{
+            if(e.target.value==element.name){
+                document.querySelector('img').src = element.img
+                document.querySelector('h4').innerText = element.name
+                document.querySelector('.date').innerText = element.birthday
+                document.querySelector('.category').innerText = element.category
+                document.querySelector('.nickname').innerText = element.nickname
+            }
+        })
+    }
+}
+getData();
